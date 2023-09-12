a78086fa8b19c4244af5c7c52098589e40d01160
Dias <65382116+diasgt123@users.noreply.github.com>
2023-09-12 20:40:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginSignup from './Components/LoginSignup/LoginSignup';
+import Scan from './Components/Scan/Scan';
 
 function App() {
   return (
     <div >
-      <LoginSignup/>
+      <Scan/>
     </div>
   );
 }
