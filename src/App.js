3070a8241e0e3da16eca493af579a3e5a7a09a10
diasgt123 <diasgt123@gmail.com>
2023-09-15 14:01:02 +0530

    commented scan

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginSignup from "./Components/LoginSignup/LoginSignup";
-import Scan from "./Components/Scan/Scan";
+//import Scan from "./Components/Scan/Scan";
 import Qr from "./Components/Qr/Qr";
 import Login from "./Components/Login/Login";
 import Signup from "./Components/Signup/Signup";
@@ -10,8 +10,8 @@ function App() {
   return (
     <div id="root">
       <Routes>
-        <Route path="/" element={<Scan />} />
-        <Route path="/Qr" element={<Qr />} />
+        <Route path="/" element={<LoginSignup />} />
+        {/* <Route path="/Qr" element={<Qr />} /> */}
         <Route path="/LoginSignup" element={<LoginSignup />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Signup" element={<Signup />} />
